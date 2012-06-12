c6ad410b03762d328c6d687f2ce461776eab099c
Nicuvëo <crucuny@gmail.com>
2012-06-12 13:16:07 +0200

    Added PP_ARRAY_SORT and PP_DISCARD.

---
+++ include/discard.hh
@@ -0,0 +1,19 @@
+//
+// discard.hh for tools
+// Made by nicuveo <crucuny@gmail.com>
+//
+
+#ifndef TOOLS_PP_DISCARD_HH_
+# define TOOLS_PP_DISCARD_HH_
+
+
+
+//HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+// Declarations
+
+# define TOOLS_PP_DISCARD(...)
+# define TOOLS_PP_ZERO(...) 0
+
+
+
+#endif /* !TOOLS_PP_DISCARD_HH_ */
